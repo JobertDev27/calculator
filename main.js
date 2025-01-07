ded89c0e7e10da3eee6f8e6ab67f9a1ca60c57c8
JobertDev27 <jobertdev27@gmail.com>
2025-01-07 20:18:36 +0800

    Added interface
    currently working on js it still barebone but the buttons work
    it's just not adding multiple numbers

---
+++ main.js
@@ -0,0 +1,25 @@
+let num1 = 0;
+let num2 = 0;
+let target;
+let opAdded = false;
+let op;
+
+const key = document.querySelectorAll(".numkey").forEach((keys) => {
+    keys.addEventListener("click", () => {
+        target = keys.textContent;
+        o
+    }
+)});
+
+const operator = document.querySelectorAll(".operator").forEach((ops) => {
+    ops.addEventListener("click", () => console.log(ops.textContent)
+)});
+
+function calculate () {
+    if (opAdded) {
+        num1 = target;
+    } else {
+        
+    }
+}
+
